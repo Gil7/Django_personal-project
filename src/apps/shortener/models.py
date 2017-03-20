c3727c95b32d39868f5df46e44d8b44e30fc2b91
gil <blackdem17@hotmail.com>
2017-03-20 12:03:02 -0600

    shortener app updatde(model)

--- src/apps/shortener/models.py
+++ src/apps/shortener/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class KirrURL(models.Model):
     url = models.CharField(max_length=220)
-
-    
+    shortcode = models.CharField(max_length=15,unique = True)
+    updated = models.DateTimeField(auto_now = True) #everytime the model is saved
+    timestamp = models.DateTimeField(auto_now_add = True) # when model was created
     def __str__(self):
         return str(self.url)
 
